a6768f839aafebb2a4e5c137c8649658251f80c6
Nicol <nicole211232@gmail.com>
2026-05-06 09:44:08 -0500

    fix: use DI singleton ViewAreaCoordinator to prevent duplicate navigation handlers

--- MVVM/ViewModels/ViewModelFactory.cs
+++ MVVM/ViewModels/ViewModelFactory.cs
@@ -47,14 +47,10 @@ namespace TestCaseEditorApp.MVVM.ViewModels
 
         public IViewAreaCoordinator CreateViewAreaCoordinator()
         {
-            var navigationMediator = CreateNavigationMediator();
-            // Don't resolve ViewConfigurationService here to avoid circular dependency
-            // It will be resolved later when actually needed
-            var sideMenuViewModel = App.ServiceProvider?.GetService<SideMenuViewModel>() 
-                ?? throw new InvalidOperationException("SideMenuViewModel not registered in DI container");
-            
-            return new ViewAreaCoordinator(this, navigationMediator, _workspaceManagementMediator!, _testCaseGenerationMediator!,
-                null, sideMenuViewModel);
+            // Always use the DI singleton to avoid multiple coordinator instances
+            // subscribing to the same NavigationMediator events.
+            return App.ServiceProvider?.GetRequiredService<IViewAreaCoordinator>()
+                ?? throw new InvalidOperationException("IViewAreaCoordinator not found in DI container");
         }
         
         public IViewConfigurationService CreateViewConfigurationService()
